8b190ef1b17d1ed2d7e2a776ccb871b76b65d39e
irinel.nistor <irinel.nistor92@gmail.com>
2019-03-19 12:26:45 +0200

    first test

---
+++ Test/ClassApp1.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ClassApp1.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
